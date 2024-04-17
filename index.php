5ac1342465eb5cfed517e1ab0456b58a9c593984
OneCooL11 <malik.ouared2003@gmail.com>
2024-04-17 12:21:36 +0200

    connexion pdo

--- index.php
+++ index.php
@@ -1,18 +1,14 @@
-Hello World
 <?php
-echo 'PHP !!!';
+try {
+    $pdo = new PDO('mysql:host=127.0.0.1;port=3307;dbname=0e5lu_a2mi_extranet', 'root', '');
+    // Configure PDO to throw exceptions on errors
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Set character set to UTF-8
+    $pdo->exec("SET CHARACTER SET utf8");
+    echo "Connected successfully to the database!";
 
-$servername = "localhost";
-$username = "root";
-$passeword = "";
-$database = "0e5lu_a2mi_extranet";
-
-try{
-    $connection = new PDO("mysql:host=$servername;dbname=$database", $username, $passeword);
-    echo "database connected !";
-
-} catch (PDOException $exception) {
-
-    echo "database not connected. " . $exception ;
-
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
+
+?>
