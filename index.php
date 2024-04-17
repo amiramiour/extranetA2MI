ba4ef69435d99e2fd6291649920de1b8ed846ad5
Amiour Amir Tahar <amir.amiour@etudiant.univ-lr.fr>
2024-04-17 12:57:07 +0100

    connexion pdo

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO('mysql:host=127.0.0.1;port=3307;dbname=0e5lu_a2mi_extranet', 'root', '');
+    $pdo = new PDO('mysql:host=127.0.0.1;port=3307;dbname=stage', 'root', '');
     // Configure PDO to throw exceptions on errors
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // Set character set to UTF-8
