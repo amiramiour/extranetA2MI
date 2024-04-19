5ccc0cf7ebdd1c4b2bee26d246b37854d65df1b4
OneCooL11 <malik.ouared2003@gmail.com>
2024-04-19 17:02:45 +0200

    Modification et avancement Bi_form

--- bi_form.php
+++ bi_form.php
@@ -3,6 +3,9 @@ global $db;
 session_start();
 header('Content-type: text/html; charset=utf-8');
 include('connexionBD.php');
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +15,8 @@ include('connexionBD.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulaire Bon d'Intervention Client</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
 </head>
 <body>
 <!-- Inclure le navbar -->
@@ -19,46 +24,56 @@ include('connexionBD.php');
 
 <div id="contenu" class="container mt-3">
     <h1>Formulaire de création d'un Bon d'Intervention client</h1>
-    <form method="post" name="SAV">
+    <form method="post" name="BI">
         <fieldset>
+            <!-- Bouton Ajouter -->
+            <button type="button" class="btn btn-primary custom-btn" id="btnAjouter" onclick="ajouterChamp()">Ajouter</button>
             <legend>BI client <small>* champs obligatoires</small></legend>
-            <div id="interventions">
+            <div id="interventions" style="flex-direction: column;">
                 <div class="intervention">
-                    <label for="selectedIntervention">Intervention/Pièces/Prix unitaire HT * </label>
-                    <input class="field" type="text" name="dynamic[1][]" id="selectedIntervention" onclick="showDropdown()" placeholder="Sélectionner une intervention" size="30" list="list_inter" required/> /
-                    <input class="field" type="text" name="dynamic[1][]" id="nbpiece_1" size="5" placeholder="Pièces" required /> /
-                    <input class="field" type="text" name="dynamic[1][]" id="prixunit_1" size="9" placeholder="Prix" required/>
-                    <div id="interventionList" class="dropdown-menu" style="display: none;">
-                        <a class="dropdown-item" href="#" onclick="selectIntervention('Forfait intervention sur site PC')">Forfait intervention sur site PC</a>
-                        <a class="dropdown-item" href="#" onclick="selectIntervention('Forfait intervention sur site MAC')">Forfait intervention sur site MAC</a>
-                        <a class="dropdown-item" href="#" onclick="selectIntervention('Forfait assistance à domicile PC')">Forfait assistance à domicile PC</a>
-                        <a class="dropdown-item" href="#" onclick="selectIntervention('Forfait assistance à domicile MAC')">Forfait assistance à domicile MAC</a>
-                        <a class="dropdown-item" href="#" onclick="selectIntervention('Formation à domicile 1H30 PC')">Formation à domicile 1H30 PC</a>
-                        <a class="dropdown-item" href="#" onclick="selectIntervention('Formation à domicile 1H30 MAC')">Formation à domicile 1H30 MAC</a>
-                        <a class="dropdown-item" href="#" onclick="selectIntervention('Formation à domicile 2H PC')">Formation à domicile 2H PC</a>
-                        <a class="dropdown-item" href="#" onclick="selectIntervention('Formation à domicile 2H MAC')">Formation à domicile 2H MAC</a>
-                        <a class="dropdown-item" href="#" onclick="selectIntervention('Forfait éradication de virus')">Forfait éradication de virus</a>
-                        <a class="dropdown-item" href="#" onclick="selectIntervention('Forfait 1H supplémentaire')">Forfait 1H supplémentaire</a>
-                        <a class="dropdown-item" href="#" onclick="selectIntervention('Forfait 2H supplémentaires')">Forfait 2H supplémentaires</a>
-                    </div>
+                    <label for="selectedIntervention" style="width: 250px;">Intervention *</label>
+                    <select id="selectedIntervention" class="form-control" onclick="showDropdown()" style="flex: 1; width: 300px;" required>
+                        <option value="" disabled selected>Sélectionner une intervention</option>
+                        <option value="Forfait intervention sur site PC">Forfait intervention sur site PC</option>
+                        <option value="Forfait intervention sur site MAC">Forfait intervention sur site MAC</option>
+                        <option value="Forfait assistance à domicile PC">Forfait assistance à domicile PC</option>
+                        <option value="Forfait assistance à domicile MAC">Forfait assistance à domicile MAC</option>
+                        <option value="Formation à domicile 1H30 PC">Formation à domicile 1H30 PC</option>
+                        <option value="Formation à domicile 1H30 MAC">Formation à domicile 1H30 MAC</option>
+                        <option value="Formation à domicile 2H PC">Formation à domicile 2H PC</option>
+                        <option value="Formation à domicile 2H MAC">Formation à domicile 2H MAC</option>
+                        <option value="Forfait éradication de virus">Forfait éradication de virus</option>
+                        <option value="Forfait 1H supplémentaire">Forfait 1H supplémentaire</option>
+                        <option value="Forfait 2H supplémentaires">Forfait 2H supplémentaires</option>
+                    </select>
+                    <label for="nbpiece_1" style="margin-right: 5px;">/ Pièces *</label>
+                    <input class="field" type="number" name="dynamic[1][]" id="nbpiece_1" size="5" placeholder="Pièces" pattern="\d+" step="1" onkeypress="return event.charCode >= 48 && event.charCode <= 57" style="width: 100px; margin-right: 5px;" required/>
+                    <label for="prixunit_1" style="margin-right: 5px;">/ Prix unitaire HT *</label>
+                    <input class="field" type="text" name="dynamic[1][]" id="prixunit_1" size="9" placeholder="Prix" pattern="[0-9]+([\.,][0-9]+)?" onkeypress="return event.charCode >= 48 && event.charCode <= 57 || event.charCode == 44 || event.charCode == 46" style="width: 150px;" required/>
+                    <button type="button" class="btn btn-danger" onclick="supprimerChamp(this)">X</button>
                 </div>
-
             </div>
-        <label>Facturer</label><br/>
+
+
             <input type="checkbox" name="facturer" value="oui"> Facturer<br/>
             <input type="checkbox" name="garantie" value="oui"> Garantie<br/>
             <input type="checkbox" name="contrat" value="oui"> Contrat/Pack<br/>
             <input type="checkbox" name="service_personne" value="oui"> Service à la personne<br/>
             <input type="checkbox" name="regle" value="oui"> Réglé<br/><br/>
 
-            <label>Envoie facture *</label><br/>
-            <input type="checkbox" name="envoie_courrier" value="oui"> Courrier<br/>
-            <input type="checkbox" name="envoie_mail" value="oui"> Mail<br/><br/>
+            <label>Envoie facture *</label
+            ><br/>
+            <input type="checkbox" name="envoie_courrier" value="oui" onchange="toggleAdresse('adresse')" /> Courrier<br/>
+            <input type="text" name="adresse" id="adresse" placeholder="Entrer votre adresse" size="60" style="display: none;">
+            <input type="checkbox" name="envoie_mail" value="oui" onchange="toggleAdresse('adresse_email')" /> Mail<br/>
+            <input type="email" name="adresse_email"  id="adresse_email" placeholder="Entrer votre adresse mail" size="25" style="display: none;">
             ( Vous pouvez cocher les deux cases ci-dessus pour facture via Courrier ET Mail )<br/><br/>
 
             <label>Facturation *</label><br/>
-            <input type="radio" name="facturation" value="immediate" required> Immédiate<br/>
-            <input type="radio" name="facturation" value="differee" required> Différée<br/><br/>
+            <input type="radio" name="facturation" value="immediate" onchange="toggleDateField()" required> Immédiate<br/>
+            <input type="radio" name="facturation" value="differee" onchange="toggleDateField()"> Différée<br/>
+            <input type="date" name="date_differee" id="date_differee" style="display: none;">
+
 
             <label>Type de paiement *</label><br/>
             <input type="radio" name="paiement" value="cb" required> CB<br/>
@@ -74,8 +89,8 @@ include('connexionBD.php');
 
             <label for="commentaire">Laisser un commentaire</label><br/>
             <textarea name="commentaire" rows="4" cols="50"></textarea><br/><br/>
-
-            <input class="createButton btn btn-primary" type="submit" value="Créer" />
+            <div id="alerteChampsVides" style="color: red;"></div>
+            <input class="createButton btn btn-primary custom-btn" type="submit" value="Créer" />
         </fieldset>
     </form>
 
@@ -89,9 +104,74 @@ include('connexionBD.php');
     function selectIntervention(intervention) {
         document.getElementById('selectedIntervention').value = intervention;
     }
+
+    function afficherResultat() {
+        var facturer = document.querySelector('input[name="facturer"]:checked') ? "oui" : "non";
+        var garantie = document.querySelector('input[name="garantie"]:checked') ? "oui" : "non";
+        var contrat = document.querySelector('input[name="contrat"]:checked') ? "oui" : "non";
+        var servicePersonne = document.querySelector('input[name="service_personne"]:checked') ? "oui" : "non";
+        var regle = document.querySelector('input[name="regle"]:checked') ? "oui" : "non";
+        var envoieCourrier = document.querySelector('input[name="envoie_courrier"]:checked') ? "courrier" : "";
+        var envoieMail = document.querySelector('input[name="envoie_mail"]:checked') ? "mail" : "";
+        var facturation = document.querySelector('input[name="facturation"]:checked').value;
+        var dateFacturation = document.getElementById("date_differee").value;
+        var paiement = document.querySelector('input[name="paiement"]:checked').value;
+        var heureArrivee = document.getElementsByName("heure_arrive")[0].value;
+        var heureDepart = document.getElementsByName("heure_depart")[0].value;
+        var commentaire = document.getElementsByName("commentaire")[0].value;
+
+        console.log("Facturer : " + facturer);
+        console.log("Garantie : " + garantie);
+        console.log("Contrat/Pack : " + contrat);
+        console.log("Service à la personne : " + servicePersonne);
+        console.log("Facture réglée : " + regle);
+        console.log("Envoyer facture par : " + (envoieCourrier || envoieMail));
+        console.log("Facturation : " + facturation);
+        console.log("Date de facturation : " + dateFacturation);
+        console.log("Type de paiement : " + paiement);
+        console.log("Heure d'arrivée : " + heureArrivee);
+        console.log("Heure de départ : " + heureDepart);
+        console.log("Commentaire : " + commentaire);
+
+        var interventions = document.querySelectorAll('.intervention');
+        var totalPrix = 0;
+        var totalPieces = 0;
+
+        interventions.forEach(function(intervention, index) {
+            var selectedIntervention = intervention.querySelector('select').value;
+            var nbPieces = parseInt(intervention.querySelector('input:nth-of-type(2)').value);
+            var prixUnitaire = parseFloat(intervention.querySelector('input:nth-of-type(3)').value);
+            var prixTotal = nbPieces * prixUnitaire;
+
+            console.log("\nIntervention : " + selectedIntervention);
+            console.log("Nb pièces : " + nbPieces + " | Prix/pièce : " + prixUnitaire.toFixed(2) + "€ | Prix total : " + prixTotal.toFixed(2) + "€");
+
+            totalPrix += prixTotal;
+            totalPieces += nbPieces;
+        });
+
+        console.log("\nTotal des pièces : " + totalPieces);
+        console.log("Total des prix : " + totalPrix.toFixed(2) + "€");
+    }
+
+    // Fonction appelée lorsque le formulaire est soumis
+    function handleSubmit(event) {
+        // Empêcher le comportement par défaut du formulaire
+        event.preventDefault();
+
+        // Afficher le résultat dans la console
+        afficherResultat();
+    }
+
+    // Ajout d'un écouteur d'événement pour le formulaire
+    document.addEventListener("DOMContentLoaded", function() {
+        var form = document.querySelector('form[name="BI"]');
+        form.addEventListener("submit", handleSubmit);
+    })
 </script>
 
 <script>
+    // liste deéroulante
     function showDropdown() {
         var inputField = document.getElementById("selectedIntervention");
         var dropdownMenu = document.getElementById("interventionList");
@@ -101,7 +181,7 @@ include('connexionBD.php');
         dropdownMenu.style.left = rect.left + "px";
         dropdownMenu.style.display = "block";
     }
-
+// selection de la liste déroulante
     function selectIntervention(intervention) {
         var inputField = document.getElementById("selectedIntervention");
         inputField.value = intervention;
@@ -115,7 +195,66 @@ include('connexionBD.php');
             dropdownMenu.style.display = "none";
         }
     });
+// Ajouter un champ d'intervention
+    var compteurChamps = 1; // Initialiser le compteur de champs
+
+    function ajouterChamp() {
+        var clone = document.querySelector('.intervention').cloneNode(true); // Cloner le champ existant
+        // Réinitialiser les valeurs des champs clonés
+        clone.querySelectorAll('input').forEach(function(input) {
+            input.value = '';
+        });
+        // Ajouter le champ cloné à la liste des interventions
+        document.getElementById('interventions').appendChild(clone);
+    }
+    // Supprimer un champ d'intervention
+    function supprimerChamp(button) {
+        var interventionsDiv = document.getElementById('interventions');
+        var interventionDiv = button.parentElement;
+        var parentDiv = interventionDiv.parentElement;
+
+        // Vérifier s'il y a plus d'un champ avant de permettre la suppression
+        if (interventionsDiv.children.length > 1) {
+            parentDiv.removeChild(interventionDiv);
+        } else {
+            alert("Vous ne pouvez pas supprimer ce champ.");
+        }
+    }
+
+
+    //Ajout d'un champ pour Courrier et Mail
+    function toggleAdresse(id) {
+        var champAdresse = document.getElementById(id);
+
+        if (champAdresse.style.display === "none") {
+            champAdresse.style.display = "block";
+        } else {
+            champAdresse.style.display = "none";
+        }
+    }
+    //Ajout d'un champ de date et récupération de la date d'aujourd'hui
+    function toggleDateField() {
+        var champDate = document.getElementById("date_differee");
+        var facturationType = document.querySelector('input[name="facturation"]:checked').value;
+
+        if (facturationType === "differee") {
+            champDate.style.display = "block";
+            champDate.required = true;
+        } else {
+            champDate.style.display = "none";
+            champDate.required = false;
+            if (facturationType === "immediate") {
+                // Remplir automatiquement le champ de la date avec la date d'aujourd'hui
+                var today = new Date();
+                var formattedDate = today.toISOString().substr(0, 10);
+                champDate.value = formattedDate;
+            }
+        }
+    }
+
+
 </script>
+
 
 
 </body>
